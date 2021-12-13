43a87a8532a0e13444c5e8efc4a3dc0763f618a1
Eleonora Petrova <eleonora.petrova@devolo.bg>
2021-12-13 14:06:27 +0200

    add time picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimePickerComponent } from './time-picker/time-picker.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimePickerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxMaterialTimepickerModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
